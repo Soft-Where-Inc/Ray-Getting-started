488cbf03407cc48732d29d4d1e8c185e9e3d50d0
Aditya Gurajada <agurajada@vmware.com>
2023-11-08 10:38:32 -0800

    Add ray_data_load.py

--- src/ray_data_load.py
+++ src/ray_data_load.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 ################################################################################
-# ray_data_scalable_datasets_for_ml.py
+# ray_data_load.py
 ################################################################################
 """
-Ray-Data: Scalable Datasets for ML - tutorial exercise.
+Ray-Data: Loading data from different sources - tutorials
+  https://docs.ray.io/en/master/data/loading-data.html#reading-files
+
+Only couple of examples are copied here. There are several more examples
+in above url for loading data into Ray using different data sources.
 """
 
 import sys
 
-from typing import Dict
-import numpy as np
+import pandas as pd
 import ray
 from utils import parse_args, fnl
 
@@ -42,37 +45,27 @@ def do_main(args) -> bool:
         print(f'do_debug = {do_debug}')
 
     # Create datasets from on-disk files, Python objects, and cloud storage like S3.
+    input_data_file = 's3://anonymous@ray-example-data/iris.parquet'
     if verbose:
-        print(fnl(), 'Download Ray data in CSV-format from S3 ...')
+        print(fnl(), f'Download Ray data in Parquet format from S3 {input_data_file}...')
 
     # Ray Data Source in CSV format
     if not dry_run:
-        rds_csv = ray.data.read_csv("s3://anonymous@ray-example-data/iris.csv")
+        rds = ray.data.read_parquet(input_data_file)
+        print(rds.schema())
 
-        # Apply functions to transform data.
-        # Ray Data executes transformations in parallel.
-        transformed_ds = rds_csv.map_batches(compute_area)
+    if verbose:
+        print(fnl(), 'Download Ray data from inline Pandas dataframe ...')
 
-        # Iterate over batches of data.
-        for one_batch in transformed_ds.iter_batches(batch_size=4):
-            print(one_batch)
+    # Ray Data Source from in-line Pandas data frame
+    if not dry_run:
+        pd_df = pd.DataFrame({
+                    "food": ["spam", "ham", "eggs"],
+                    "price": [9.34, 5.37, 0.94]
+                    })
+        rds = ray.data.from_pandas(pd_df)
+        print(rds)
 
-    # Save dataset contents to on-disk files or cloud storage.
-    outfile = 'local:///tmp/iris/'
-    save_verb = 'Saved'
-    if dry_run:
-        save_verb = 'Save'
-    else:
-        transformed_ds.write_parquet(outfile)
-    print(fnl(), f"{save_verb} data set contents to output file: '{outfile}'")
-
-################################################################################
-def compute_area(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
-    """Compute the area given a batch of NumPy-arrays."""
-    length = batch["petal length (cm)"]
-    width = batch["petal width (cm)"]
-    batch["petal area (cm^2)"] = length * width
-    return batch
 
 ###############################################################################
 # Start of the script: Execute only if run as a script
