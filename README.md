058e1a425e6529d063fc77a4cb5ae006a04cab4c
Aditya Gurajada <agurajada@vmware.com>
2023-12-05 13:11:56 -0800

    Initial version of ray_crash_course_tasks_pll.py

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Python exercises to get started with Ray, using online training exercises.
   - Ray Cluster Quickstart
 
 - Ray [Building Ray](https://docs.ray.io/en/master/ray-contribute/development.html#building-ray) how-to
+
+- Ray [Academy](https://github.com/anyscale/academy/tree/ebd151134127168162c1175ed1c7da979e475a83/ray-crash-course) Crash course
---
+++ src/ray_crash_course_tasks_pll.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+################################################################################
+# ray_crash_course_tasks_pll.py
+################################################################################
+"""
+Ray Crash Course: Tasks and remote execution.
+
+In this example, if a Ray cluster is already up, ray_init() will connect to it.
+Otherwise, it will start, something called, a local cluster.
+
+To start a cluster manually:
+
+    $ ray start --head --port=6379 --object-manager-port=8076 --dashboard-host=0.0.0.0
+
+    # Cut-n-paste below, the address reported by above command.
+    $ ray start --address='10.212.239.203:6379' # Repeat n-times
+
+# pylint: disable=line-too-long
+Refs: https://github.com/anyscale/academy/blob/ebd151134127168162c1175ed1c7da979e475a83/ray-crash-course/01-Ray-Tasks.ipynb
+# pylint: enable=line-too-long
+"""
+
+import sys
+import ray
+from utils import parse_args, fnl # pylint: disable=unused-import
+
+###############################################################################
+def regular_function():
+    """Return function's code-location"""
+    return fnl()
+
+# A Ray remote function.
+@ray.remote
+def remote_function():
+    """Return constant value"""
+    return 1
+
+###############################################################################
+def main():
+    """
+    Shell to call do_main() with command-line arguments.
+    """
+    do_main(sys.argv[1:])
+
+###############################################################################
+def do_main(args) -> bool:
+    """
+    Main driver to implement argument processing.
+    """
+    # Parsed args and extract cmdline flags into local variables
+    parsed_args = parse_args(args, 'Ray - Remote execution of a method')
+    verbose     = parsed_args.verbose
+    dry_run     = parsed_args.dry_run
+    do_debug    = parsed_args.debug_script
+    dump_flag   = parsed_args.dump_flags
+
+    # Dump args, also shuts-up pylint ...
+    if dump_flag:
+        print(f'verbose = {verbose}')
+        print(f'do_debug = {do_debug}')
+
+    if not dry_run:
+        print('Return from regular_function:' + regular_function())
+        ray.init()
+
+        # Let's invoke the remote regular function.
+        remf_rv = ray.get(remote_function.remote())
+        print(f'Return from remote function invocation {remf_rv}')
+
+###############################################################################
+# Start of the script: Execute only if run as a script
+###############################################################################
+if __name__ == "__main__":
+    main()
